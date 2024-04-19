8220559f3d50e2b8ecc36e7e9200ae9014937f48
Divyansh Pratap <dipratap@adobe.com>
2024-04-20 01:54:25 +0530

    Updates block: Cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,32 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
+function appendArrowImagesToAnchors() {
+  const aTags = document.querySelectorAll('.cards-wrapper .cards-card-body .button-container a');
+  aTags.forEach((a) => {
+    const { innerText } = a;
+    a.innerHTML = '';
+    const span = document.createElement('span');
+    span.innerText = innerText;
+    const img = document.createElement('img');
+    img.src = '/icons/white-arrow-right.png';
+    img.alt = 'Right Arrow';
+    a.append(span, img);
+  });
+}
+
+function validateAndCorrectCardsText() {
+  const cardBodyElements = document.querySelectorAll('.cards-wrapper .cards-card-body');
+  cardBodyElements.forEach((cardBody) => {
+    if (cardBody.children.length === 2 && cardBody.querySelector('p strong')) {
+      const description = cardBody.querySelector('strong').textContent;
+      cardBody.querySelector('strong').remove();
+      const strongTag = document.createElement('strong');
+      strongTag.textContent = description;
+      cardBody.insertBefore(strongTag, cardBody.children[1]);
+    }
+  });
+}
+
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
@@ -15,4 +42,6 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+  appendArrowImagesToAnchors();
+  validateAndCorrectCardsText();
 }

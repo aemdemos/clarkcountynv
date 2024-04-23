8b928003d468f2c770e74934be25f3b6a0b5c45d
Divyansh Pratap <dipratap@adobe.com>
2024-04-24 01:21:12 +0530

    Fix accessibility icon

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -7,47 +7,33 @@ sampleRUM('cwv');
 // add more delayed functionality here
 
 async function loadWidget() {
-  await loadScript(
-    '/scripts/clientlibs/widget.js',
-  );
+
   await loadCSS(
     '/scripts/clientlibs/widget.css',
   );
-  // .then(() => {
-  //   console.log('---------------------------------script loaded');
-  //   // wait for 1 second before accessing the iframe
-  //   setTimeout(() => {
-  //     console.log('---------------------------------1 second passed');
-  //     const iframe = document.querySelector('iframe');
-  //
-  //     // Add a 'load' event listener to the iframe
-  //     iframe.addEventListener('load', () => {
-  //       console.log('Iframe loaded successfully!');
-  //
-  //       // Access the iframe content to set CSS styles
-  //       const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-  //       const mainElement = iframeDocument.querySelector('main');
-  //
-  //       // Example: Set CSS styles for the main element inside the iframe
-  //       mainElement.style.backgroundColor = 'lightblue';
-  //       mainElement.style.padding = '20px';
-  //     });
-  //   }, 5000);
-  //   // const iframe = document.querySelector('iframe');
-  //   //
-  //   // // Add a 'load' event listener to the iframe
-  //   // iframe.addEventListener('load', () => {
-  //   //   console.log('Iframe loaded successfully!');
-  //   //
-  //   //   // Access the iframe content to set CSS styles
-  //   //   const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-  //   //   const mainElement = iframeDocument.querySelector('main');
-  //   //
-  //   //   // Example: Set CSS styles for the main element inside the iframe
-  //   //   mainElement.style.backgroundColor = 'lightblue';
-  //   //   mainElement.style.padding = '20px';
-  //   // });
-  // });
+  await loadScript(
+    '/scripts/clientlibs/widget.js',
+  ).then(() => {
+    console.log('---------------------------------script loaded');
+    // wait for 1 second before accessing the iframe
+    setTimeout(() => {
+      console.log('---------------------------------1 second passed');
+      const iframe = document.querySelector('iframe');
+
+      // Add a 'load' event listener to the iframe
+      iframe.addEventListener('load', () => {
+        console.log('Iframe loaded successfully!');
+
+        // Access the iframe content to set CSS styles
+        const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+        const mainElement = iframeDocument.querySelector('main');
+
+        // Example: Set CSS styles for the main element inside the iframe
+        mainElement.style.backgroundColor = 'lightblue';
+        mainElement.style.right = 'unset';
+      });
+    }, 5000);
+  });
 }
 
 loadWidget();

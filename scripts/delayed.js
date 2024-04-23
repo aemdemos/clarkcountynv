01aece70d1508a8f03ae82ed0d70d4366d965704
Divyansh Pratap <dipratap@adobe.com>
2024-04-24 01:18:27 +0530

    Fix accessibility icon

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -8,8 +8,46 @@ sampleRUM('cwv');
 
 async function loadWidget() {
   await loadScript(
-    `https://cdn.userway.org/widget.js`,
+    '/scripts/clientlibs/widget.js',
   );
+  await loadCSS(
+    '/scripts/clientlibs/widget.css',
+  );
+  // .then(() => {
+  //   console.log('---------------------------------script loaded');
+  //   // wait for 1 second before accessing the iframe
+  //   setTimeout(() => {
+  //     console.log('---------------------------------1 second passed');
+  //     const iframe = document.querySelector('iframe');
+  //
+  //     // Add a 'load' event listener to the iframe
+  //     iframe.addEventListener('load', () => {
+  //       console.log('Iframe loaded successfully!');
+  //
+  //       // Access the iframe content to set CSS styles
+  //       const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+  //       const mainElement = iframeDocument.querySelector('main');
+  //
+  //       // Example: Set CSS styles for the main element inside the iframe
+  //       mainElement.style.backgroundColor = 'lightblue';
+  //       mainElement.style.padding = '20px';
+  //     });
+  //   }, 5000);
+  //   // const iframe = document.querySelector('iframe');
+  //   //
+  //   // // Add a 'load' event listener to the iframe
+  //   // iframe.addEventListener('load', () => {
+  //   //   console.log('Iframe loaded successfully!');
+  //   //
+  //   //   // Access the iframe content to set CSS styles
+  //   //   const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+  //   //   const mainElement = iframeDocument.querySelector('main');
+  //   //
+  //   //   // Example: Set CSS styles for the main element inside the iframe
+  //   //   mainElement.style.backgroundColor = 'lightblue';
+  //   //   mainElement.style.padding = '20px';
+  //   // });
+  // });
 }
 
 loadWidget();

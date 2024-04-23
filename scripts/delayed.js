1dc60d0239feec4b0961a017d9ac452581638143
Divyansh Pratap <dipratap@adobe.com>
2024-04-24 01:42:37 +0530

    Fix accessibility icon

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-cycle
-import { loadScript, loadCSS, sampleRUM } from './aem.js';
+import { loadScript, sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
@@ -7,25 +7,15 @@ sampleRUM('cwv');
 // add more delayed functionality here
 
 async function loadWidget() {
-
-  await loadCSS(
-    '/scripts/clientlibs/widget.css',
-  );
   await loadScript(
-    '/scripts/clientlibs/widget.js',
+    'https://cdn.userway.org/widget.js',
   ).then(() => {
     console.log('---------------------------------script loaded');
     // wait for 1 second before accessing the iframe
     setTimeout(() => {
       console.log('---------------------------------1 second passed');
       const iframe = document.querySelector('iframe');
-
-      // Add a 'load' event listener to the iframe
-      iframe.addEventListener('load', () => {
-        console.log('Iframe loaded successfully!');
-
-        iframe.style['max-width'] = '25vw';
-      });
+      iframe.style.maxWidth = '22vw';
     }, 5000);
   });
 }

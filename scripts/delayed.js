3a63804407b61982ab5c5487505b52fdb412042c
Divyansh Pratap <dipratap@adobe.com>
2024-04-24 15:44:02 +0530

    fix: accessibility icon color

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -19,6 +19,8 @@ function applyBackground(counter) {
   const div = document.querySelector('.userway_buttons_wrapper .uai.userway_dark');
   if (div) {
     div.style.setProperty('background', 'rgb(0, 148, 195)', 'important');
+    const widget = document.querySelector('body > div.uwy.userway_p1.utb > div');
+    widget.style.opacity = '1';
   } else if (counter < 5000) {
     setTimeout(() => applyBackground(counter + 1), 5);
   }
